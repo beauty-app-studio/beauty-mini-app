9729f1149b027358575154bccace37ca06e5573a
beauty-app-studio <dolusmalus14@gmail.com>
2026-08-09 20:55:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const API_BASE_URL =
-  "https://mode-observer-course-procedure.trycloudflare.com";
+  "https://housewares-inspections-handling-votes.trycloudflare.com";
 
 const masters = {
   nails: {
